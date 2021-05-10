aec181a9a649ff49fc3cd81509390efc537bba28
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2021-05-10 14:32:47 -0400

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>
