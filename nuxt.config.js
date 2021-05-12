8f937dc6f252fc7eb89c2a576270e7bc3dc614af
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2021-05-12 13:42:11 -0400

    Update Nuxt Config to include GoogleFonts plugin with fonts and options, subfolder component paths for auto importing, pug plain loader and babel build configs. Download selected fonts and css from Google plugin build.

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,16 +8,31 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
   vue: {
     config: {
-      devtools: true
-    }
+      devtools: true,
+    },
   },
-
+  // Google Fonts Import: https://google-fonts.nuxtjs.org/options
+  googleFonts: {
+    families: {
+      'Titillium+Web': {
+        wght: [700, 900],
+      },
+      Mulish: {
+        wght: [500, 700],
+      },
+    },
+    // preconnect: true,
+    download: true,
+    fontsDir: 'fonts',
+    overwriting: true,
+    display: 'swap',
+  },
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
@@ -25,29 +40,58 @@ export default {
   plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: [
+    '~/components/',
+    {
+      path: '~/components/atoms/',
+      prefix: 'A-',
+    },
+    {
+      path: '~/components/molecules',
+      prefix: 'M-',
+    },
+    {
+      path: '~/components/organisms',
+      prefix: 'O-',
+    },
+  ],
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    // https://google-fonts.nuxtjs.org/
+    '@nuxtjs/google-fonts',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
   ],
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
-    }
+      lang: 'en',
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {}
+  build: {
+    extend(config) {
+      config.module.rules.push({
+        test: /\.pug$/,
+        loader: 'pug-plain-loader',
+        options: {
+          data: {},
+        },
+      });
+    },
+    babel: {
+      plugins: [['@babel/plugin-proposal-private-methods', { loose: true }]],
+    },
+  },
 };
