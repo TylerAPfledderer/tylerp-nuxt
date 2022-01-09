e24ab2c40c1c3ef62a2a1ccf72dc00f2c5b618e5
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2022-01-09 17:17:35 -0500

    feat: Import the content api to the `SkillList` component and call the 'skills' collection markdown files, remove the hard-coded details from the setup, and update the props in the `SkillCard` component to reflect this change.
    - Import 'useAsync' and 'useContext' from '@nuxtjs/composition-api'
    - Grab '$content' from 'useContext' via destructuring in setup.
    - Replace hard coded array in the object 'skills' with the async call to content, getting the markdown files from the 'skills' collection and sort list by the label 'display-order'.
    - Change prop names in the 'SkillCard' app to reflect the names provided by the collection.

--- components/organisms/SkillList.vue
+++ components/organisms/SkillList.vue
@@ -8,52 +8,16 @@
       M-SkillCard(:skill='skill')
 </template>
 <script>
+import { useAsync, useContext } from '@nuxtjs/composition-api';
 export default {
   setup() {
-    const skills = [
-      {
-        logos: ['universal-access'],
-        heading: 'Web Accessibility',
-        description:
-          'Attention to detail with the use of the NVDA screen reader to test interactions with web pages and components. This is essential for universal usuability and design.',
-      },
-      {
-        logos: ['react'],
-        heading: 'Quick Building With React',
-        description:
-          'With my fundamental knowledge of React and the Gatsby framework, they can provide high usability to your complex project.',
-      },
-      {
-        logos: ['vuejs', 'nuxt'],
-        heading: 'Or Maybe You Prefer Vue?',
-        description:
-          'Vue can provide you a lighter load for a smaller application.<br/> <strong>(This very site is built in NuxtJS!)</strong>',
-      },
-      {
-        logos: ['sass'],
-        heading: 'Preprocessing with SASS',
-        description:
-          'This matters in making stylesheet creation programmatic . . . Which means fast building and scaling of the design!',
-      },
-      {
-        logos: ['tailwindcss', 'bootstrap'],
-        heading: 'CSS Frameworks',
-        description:
-          'Knowledge with Bootstrap and TailwindCSS to provide quick UI layouts and Utility styling. <br /> <strong>(This site implements TailwindCSS)</strong>',
-      },
-      {
-        logos: ['html', 'css', 'js'],
-        heading: "Don't Forget the Basics!",
-        description: 'Proficient in HTML, CSS, and JavaScript. Simple.',
-      },
-      {
-        logos: ['laptop-code'],
-        heading: 'Code Review',
-        description:
-          "It's just as important to keep attention to detail on a team to ensure everyone succeeds!",
-      },
-    ];
-
+    const { $content } = useContext();
+    const skills = useAsync(() => {
+      return $content('skills')
+        .only(['display-order', 'title', 'description', 'tags'])
+        .sortBy('display-order', 'asc')
+        .fetch();
+    });
     return { skills };
   },
 };
